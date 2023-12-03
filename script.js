d85a294e98b45ee050f4fece7a44d846516304d1
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2023-12-03 11:51:51 +0100

    Create getComputerChoice() from given pseudocode
    Function returns random item from an OPTIONS array, that contains
    'Rock', 'Paper' and 'Scissors' strings

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 // it has to have 3 strings - rock, paper and scissors
 // so creating an array may be a good option
 // strings have to be returned randomly
-// So I'm gonna make random number between 0 - 2 and return string from arrayw with given random ID
+// So I'm gonna make random number between 0 - 2 and return string from array with given random ID
 
+function getComputerChoice() {
+  const OPTIONS = ["Rock", "Paper", "Scissors"];
+  let randomIndex = Math.floor(Math.random() * 3);
+  return OPTIONS[randomIndex];
+}
