0399f7c07b5f54206b2cdd5c2b83bb8bae72fcd4
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2023-12-24 11:45:05 +0100

    Add reset button and winner message

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll(".play-buttons > button");
 const roundResult = document.querySelector('.round-result');
 const playerScoreDisplay = document.querySelector('.player-score');
 const computerScoreDisplay = document.querySelector('.computer-score');
+const resetButton = document.querySelector('#reset-button');
+const winnerMessage = document.querySelector('.winner-message');
 
 let playerScore = 0;
 let computerScore = 0;
@@ -24,6 +26,15 @@ for (let button of buttons) {
     checkGameEnd();
   });
 }
+
+resetButton.addEventListener('click', () => {
+  playerScore = 0;
+  computerScore = 0;
+  buttons.forEach((button) => button.disabled = false);
+  displayScore();
+  roundResult.textContent = 'Make your choice!';
+  winnerMessage.textContent = '';
+});
 
 function getComputerChoice() {
   const OPTIONS = ['Rock', 'Paper', 'Scissors'];
@@ -56,5 +67,10 @@ function displayScore() {
 function checkGameEnd() {
   if (playerScore === 5 || computerScore === 5) {
     buttons.forEach((button) => button.disabled = true);
+    if (playerScore === 5) {
+      winnerMessage.textContent = 'Congratulations! You are the winner!'
+    } else if (computerScore === 5) {
+      winnerMessage.textContent = 'Unfortunately the computer beats you. :('
+    }
   }
 }
